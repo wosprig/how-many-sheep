4085303eeb9a30cac574d9c911eb1749079c5ed3
Rachel Anderson <andersrach8@ecs.vuw.ac.nz>
2018-08-26 14:44:15 +1200

    map colours

--- js/index.js
+++ js/index.js
@@ -1,13 +1,34 @@
-/*
- * We are using jQuery functions as they are pretty easy to use.
- * You can do all of this stuff using vanilla javascript though
- */
-
-// $(...) will run the function you give it when the page is loaded & ready
-
 var num_humans, num_sheep;
 
-loadpage("Auckland");
+var regionnames = ["Chatham Islands",
+  "West Coast",
+  "Marlborough",
+  "Gisborne",
+  "Nelson",
+  "Tasman",
+  "Southland",
+  "Taranaki",
+  "Hawkes Bay",
+  "Northland",
+  "Otago",
+  "Manawatu-Wanganui",
+  "Bay of Plenty",
+  "Waikato",
+  "Wellington",
+  "Canterbury",
+  "Auckland"
+];
+
+var ratios = [];
+
+
+$(function() { 
+  onload();
+  setTimeout(function(){
+    setmapcolors(); 
+  }, 1000);
+  
+})
 
 function loadpage(region) {
   fetch("/humans/" + region).then((data) => data.json()).then((data) => { 
@@ -19,48 +40,69 @@ function loadpage(region) {
       document.getElementById('num_sheep').innerHTML = format_num(data); 
     }).then(() => {
       var ratio = num_sheep / num_humans;
+      ratios[region] = ratio.toFixed(1);
       document.getElementById('ratio').innerHTML = ratio.toFixed(1); 
       document.getElementById('region').innerHTML = region;
+      document.getElementById('sheep').innerHTML = "";
 
-      while(ratio >= 1) {
+      var tempratio = ratio;
+      while(tempratio >= 1) {
         var temp, item, a;
         temp = document.getElementsByTagName("template")[0];
         item = temp.content.querySelector("div");
         a = document.importNode(item, true);
         a.style.width = '100px';
         document.getElementById('sheep').appendChild(a);
-        ratio--;
+        tempratio--;
       }
       var temp, item, a;
       temp = document.getElementsByTagName("template")[0];
       item = temp.content.querySelector("div");
       a = document.importNode(item, true);
-      a.style.width = (100 * ratio.toFixed(1)).toString() + 'px';
+      a.style.width = (100 * tempratio.toFixed(1)).toString() + 'px';
       document.getElementById('sheep').appendChild(a);
     });
   });
 }
 
+function onload() {
+  for(var i = 0; i < regionnames.length; i++) {
+    loadpage(regionnames[i]);
+  }
+  loadpage("New Zealand");
+}
+
+
+function setmapcolors() {
+  var maps = document.getElementsByTagName('path');
+
+  for(var i = 0; i < maps.length; i++) {
+    var region = maps[i].dataset.title;
+    console.log(ratios);
+
+    if(ratios[region] < 2) {
+      maps[i].classList.add('ratio-1');
+    }
+    if(ratios[region] <= 5 && ratios[region] >= 2) {
+      maps[i].classList.add('ratio-2');
+    }
+    if(ratios[region] <= 11 && ratios[region] > 5) {
+      maps[i].classList.add('ratio-3');
+    }
+    if(ratios[region] <= 23 && ratios[region] > 11) {
+      maps[i].classList.add('ratio-4');
+    }
+    if(ratios[region] <= 42 && ratios[region] > 23) {
+      maps[i].classList.add('ratio-5');
+    }
+    if(ratios[region] > 42) {
+      maps[i].classList.add('ratio-6');
+    }
+  }
+}
 
 
 const format_num = (x) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-$(function() {
-
-  var imageCountPeople = 1;
-  var imageCountSheep = 6;
-  //varimage = 
-  
-  var paragraph = document.getElementById('images');
-  for (var i = 0; i < 6; i++) {
-      paragraph.innerHTML += "<img src='person.svg'>";
-  }
-  // console.log will log a message or object to the browser developer console
-  
-  
-  document.getElementById('injected').innerHTML = "I INJECTED THIS";
-  
-  console.log("page loaded...");
-});
